776c89f184851f97113541440effa19380b21509
athanop <elnahue.-@live.com.ar>
2023-11-27 22:21:32 -0300

    mas ajustes, checkpoint

--- src/components/organisms/Tablero.jsx
+++ src/components/organisms/Tablero.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './Tablero.css';
+import style from './Tablero.css';
 import IconPortaAviones from '../atoms/portaaviones.png';
 import IconCrucero from '../atoms/crucero.png';
 import IconLancha from '../atoms/lancha.png';
@@ -9,156 +9,160 @@ import Letras from '../molecules/Letras';
 import Numeros from '../molecules/Numeros';
 
 
-const Tablero = ({esJugador, randomShips}) => {
-    const [celdasValidas, setCeldasValidas] = useState([]);
-    const [celdasInvalidas, setCeldasInvalidas] = useState([]);
-    const [tableroJugador, setTableroJugador] = useState(Array.from({ length: 10 }, () => Array(10).fill(null)));
-    const [fichaArrastrada, setFichaArrastrada] = useState(null);
-    const [orientacionIconos, setOrientacionIconos] = useState('horizontal');
-    const [fichasDisponibles, setFichasDisponibles] = useState({
-        [IconPortaAviones]: 1,
-        [IconCrucero]: 1,
-        [IconLancha]: 1,
-        [IconSubmarino]: 1,
-    });
-    const barcosColocados = Object.values(fichasDisponibles).every((cantidad) => cantidad === 0);
-    const [barcosBloqueados, setBarcosBloqueados] = useState(false); 
-   
-    useEffect(() => {
-      if (!esJugador) {
-        setTableroJugador(randomShips);
-      }
-    }, [esJugador, randomShips]);
-    
-    const bloquearBarcos = () => {
-      if (barcosColocados) {
-        setBarcosBloqueados(true);
-      } else {
-        console.log('Aún faltan barcos por colocar');
-      }
-    };
+const Tablero = ({ esJugador, randomShips }) => {
+  const [celdaAtacada, setCeldaAtacada] = useState(null);
+  const [celdasValidas, setCeldasValidas] = useState([]);
+  const [celdasInvalidas, setCeldasInvalidas] = useState([]);
+  const [tableroJugador, setTableroJugador] = useState(Array.from({ length: 10 }, () => Array(10).fill(null)));
+  const [fichaArrastrada, setFichaArrastrada] = useState(null);
+  const [orientacionIconos, setOrientacionIconos] = useState('horizontal');
+  const [fichasDisponibles, setFichasDisponibles] = useState({
+    [IconPortaAviones]: 1,
+    [IconCrucero]: 1,
+    [IconLancha]: 1,
+    [IconSubmarino]: 1,
+  });
+  const barcosColocados = Object.values(fichasDisponibles).every((cantidad) => cantidad === 0);
+  const [barcosBloqueados, setBarcosBloqueados] = useState(false);
 
-    const cambiarOrientacion = (orientacion) => {
-        setOrientacionIconos(orientacion);
-    };
+  useEffect(() => {
+    if (!esJugador) {
+      setTableroJugador(randomShips);
+    }
+  }, [esJugador, randomShips]);
 
-    const handleDragStart = (event, ficha) => {
-        if (fichasDisponibles[ficha.icono] > 0) {
-            setFichaArrastrada(ficha);
-        }
-    };
-
-    const handleDragOver = (event, filaIndex, celdaIndex) => {
-        event.preventDefault();
-
-        if (!esJugador || !fichaArrastrada) return;
-
-        const ficha = { ...fichaArrastrada };
-        const size = ficha.agujeros;
-
-        const maxFilaIndex = orientacionIconos === 'horizontal' ? filaIndex : filaIndex + size - 1;
-        const maxCeldaIndex = orientacionIconos === 'horizontal' ? celdaIndex + size - 1 : celdaIndex;
-
-        const celdasValidas = [];
-        let celdasInvalidas = [];
-
-        if (maxFilaIndex < 10 && maxCeldaIndex < 10) {
-            for (let i = filaIndex; i <= maxFilaIndex; i++) {
-                for (let j = celdaIndex; j <= maxCeldaIndex; j++) {
-                    if (tableroJugador[i][j] === null) {
-                        celdasValidas.push({ fila: i, celda: j });
-                    } else {
-                        celdasInvalidas.push({ fila: i, celda: j });
-                    }
-                }
-            }
-        } else {
-            celdasInvalidas = [{ fila: filaIndex, celda: celdaIndex }];
-        }
-
-        setCeldasValidas(celdasValidas);
-        setCeldasInvalidas(celdasInvalidas);
-    };
-
-    const handleDrop = (event, filaIndex, celdaIndex) => {
-        event.preventDefault();
-        if (!esJugador || !fichaArrastrada || barcosBloqueados) return;
-
-        const nuevoTablero = JSON.parse(JSON.stringify(tableroJugador));
-        const ficha = { ...fichaArrastrada };
-        const size = ficha.agujeros;
-
-        const maxFilaIndex = orientacionIconos === 'horizontal' ? filaIndex : filaIndex + size - 1;
-        const maxCeldaIndex = orientacionIconos === 'horizontal' ? celdaIndex + size - 1 : celdaIndex;
-
-        if (maxFilaIndex >= 10 || maxCeldaIndex >= 10) {
-            setFichaArrastrada(null);
-            setCeldasValidas([]);
-            setCeldasInvalidas([]);
-            return;
-        }
-
-        for (let i = filaIndex; i <= maxFilaIndex; i++) {
-            for (let j = celdaIndex; j <= maxCeldaIndex; j++) {
-                if (nuevoTablero[i][j] !== null) {
-                    setFichaArrastrada(null);
-                    setCeldasValidas([]);
-                    setCeldasInvalidas([]);
-                    return;
-                }
-            }
-        }
-
-        if (fichasDisponibles[ficha.icono] > 0) {
-            for (let i = filaIndex; i <= maxFilaIndex; i++) {
-                for (let j = celdaIndex; j <= maxCeldaIndex; j++) {
-                    nuevoTablero[i][j] = { icono: ficha.icono };
-                }
-            }
-
-            const nuevasFichasDisponibles = {
-                ...fichasDisponibles,
-                [ficha.icono]: fichasDisponibles[ficha.icono] - 1,
-            };
-
-            setFichasDisponibles(nuevasFichasDisponibles);
-            setTableroJugador(nuevoTablero);
-            setFichaArrastrada(null);
-            setCeldasValidas([]);
-        }
-    };
-
-
-    const borrarBarco = (filaIndex, celdaIndex) => {
-      const tableroActualizado = JSON.parse(JSON.stringify(tableroJugador));
-      const celda = tableroActualizado[filaIndex][celdaIndex];
-  
-      if (!celda || !celda.icono) return;
-  
-      tableroActualizado[filaIndex][celdaIndex] = null;
-  
-      const iconoBarco = celda.icono;
-  
-      const barcoEnCelda = tableroActualizado.some((fila) =>
-          fila.some((c) => c && c.icono === iconoBarco)
-      );
-  
-      if (!barcoEnCelda) {
-          const nuevasFichasDisponibles = {
-              ...fichasDisponibles,
-              [iconoBarco]: fichasDisponibles[iconoBarco] + 1,
-          };
-  
-          setFichasDisponibles(nuevasFichasDisponibles);
-      }
-  
-      setTableroJugador(tableroActualizado);
+  const bloquearBarcos = () => {
+    if (barcosColocados) {
+      setBarcosBloqueados(true);
+    } else {
+      console.log('Aún faltan barcos por colocar');
+    }
   };
-      
+
+  const cambiarOrientacion = (orientacion) => {
+    setOrientacionIconos(orientacion);
+  };
+
+  const handleDragStart = (event, ficha) => {
+    if (fichasDisponibles[ficha.icono] > 0) {
+      setFichaArrastrada(ficha);
+    }
+  };
+
+  const handleDragOver = (event, filaIndex, celdaIndex) => {
+    event.preventDefault();
+
+    if (!esJugador || !fichaArrastrada) return;
+
+    const ficha = { ...fichaArrastrada };
+    const size = ficha.agujeros;
+
+    const maxFilaIndex = orientacionIconos === 'horizontal' ? filaIndex : filaIndex + size - 1;
+    const maxCeldaIndex = orientacionIconos === 'horizontal' ? celdaIndex + size - 1 : celdaIndex;
+
+    const celdasValidas = [];
+    let celdasInvalidas = [];
+
+    if (maxFilaIndex < 10 && maxCeldaIndex < 10) {
+      for (let i = filaIndex; i <= maxFilaIndex; i++) {
+        for (let j = celdaIndex; j <= maxCeldaIndex; j++) {
+          if (tableroJugador[i][j] === null) {
+            celdasValidas.push({ fila: i, celda: j });
+          } else {
+            celdasInvalidas.push({ fila: i, celda: j });
+          }
+        }
+      }
+    } else {
+      celdasInvalidas = [{ fila: filaIndex, celda: celdaIndex }];
+    }
+
+    setCeldasValidas(celdasValidas);
+    setCeldasInvalidas(celdasInvalidas);
+  };
+
+  const handleDrop = (event, filaIndex, celdaIndex) => {
+    event.preventDefault();
+    if (!esJugador || !fichaArrastrada || barcosBloqueados) return;
+
+    const nuevoTablero = JSON.parse(JSON.stringify(tableroJugador));
+    const ficha = { ...fichaArrastrada };
+    const size = ficha.agujeros;
+
+    const maxFilaIndex = orientacionIconos === 'horizontal' ? filaIndex : filaIndex + size - 1;
+    const maxCeldaIndex = orientacionIconos === 'horizontal' ? celdaIndex + size - 1 : celdaIndex;
+
+    if (maxFilaIndex >= 10 || maxCeldaIndex >= 10) {
+      setFichaArrastrada(null);
+      setCeldasValidas([]);
+      setCeldasInvalidas([]);
+      return;
+    }
+
+    for (let i = filaIndex; i <= maxFilaIndex; i++) {
+      for (let j = celdaIndex; j <= maxCeldaIndex; j++) {
+        if (nuevoTablero[i][j] !== null) {
+          setFichaArrastrada(null);
+          setCeldasValidas([]);
+          setCeldasInvalidas([]);
+          return;
+        }
+      }
+    }
+
+    if (fichasDisponibles[ficha.icono] > 0) {
+      for (let i = filaIndex; i <= maxFilaIndex; i++) {
+        for (let j = celdaIndex; j <= maxCeldaIndex; j++) {
+          nuevoTablero[i][j] = { icono: ficha.icono };
+        }
+      }
+
+      const nuevasFichasDisponibles = {
+        ...fichasDisponibles,
+        [ficha.icono]: fichasDisponibles[ficha.icono] - 1,
+      };
+
+      setFichasDisponibles(nuevasFichasDisponibles);
+      setTableroJugador(nuevoTablero);
+      setFichaArrastrada(null);
+      setCeldasValidas([]);
+    }
+  };
+
+
+  const borrarBarco = (filaIndex, celdaIndex) => {
+    const tableroActualizado = JSON.parse(JSON.stringify(tableroJugador));
+    const celda = tableroActualizado[filaIndex][celdaIndex];
+
+    if (!celda || !celda.icono) return;
+
+    tableroActualizado[filaIndex][celdaIndex] = null;
+
+    const iconoBarco = celda.icono;
+
+    const barcoEnCelda = tableroActualizado.some((fila) =>
+      fila.some((c) => c && c.icono === iconoBarco)
+    );
+
+    if (!barcoEnCelda) {
+      const nuevasFichasDisponibles = {
+        ...fichasDisponibles,
+        [iconoBarco]: fichasDisponibles[iconoBarco] + 1,
+      };
+
+      setFichasDisponibles(nuevasFichasDisponibles);
+    }
+
+    setTableroJugador(tableroActualizado);
+  };
+
+  const letras = Array.from({ length: tableroJugador.length }, (_, index) => String.fromCharCode(65 + index));
+  const numeros = Array.from({ length: tableroJugador[0].length }, (_, index) => index + 1);
+
   const handleCellClick = (filaIndex, celdaIndex) => {
     if (!barcosBloqueados) {
       const iconoBarco = tableroJugador[filaIndex][celdaIndex]?.icono;
-  
+
       if (iconoBarco) {
         borrarBarco(filaIndex, celdaIndex);
       } else if (!esJugador) {
@@ -167,44 +171,51 @@ const Tablero = ({esJugador, randomShips}) => {
       } else {
         borrarBarco(filaIndex, celdaIndex);
       }
+      const letra = letras[filaIndex];
+      const numero = numeros[celdaIndex];
+
+      setCeldaAtacada(`${letra}${numero}`);
     }
   };
-  
-  
+
+
   const pintarCeldaRoja = (filaIndex, celdaIndex) => {
-    if (!esJugador) { 
+    if (!esJugador) {
       const updatedTablero = [...tableroJugador];
       updatedTablero[filaIndex] = [...updatedTablero[filaIndex]];
-  
+
       updatedTablero[filaIndex][celdaIndex] = {
         ...updatedTablero[filaIndex][celdaIndex],
         sinBarco: true,
       };
-  
+
       setTableroJugador(updatedTablero);
     }
   };
-  
+
   const bloquearCelda = (filaIndex, celdaIndex) => {
-    if (!esJugador) { 
+    if (!esJugador) {
       const updatedCeldasInvalidas = [...celdasInvalidas];
       updatedCeldasInvalidas.push({ fila: filaIndex, celda: celdaIndex });
       setCeldasInvalidas(updatedCeldasInvalidas);
     }
   };
-  
 
-  const letras = Array.from({ length: tableroJugador.length }, (_, index) => String.fromCharCode(65 + index));
-  const numeros = Array.from({ length: tableroJugador[0].length }, (_, index) => index + 1);
 
 
   return (
     <div className="contenedor-general">
+      <div className="info-celda-atacada" style={style}>
+        {celdaAtacada && (
+          <p>El disparo ocurrió en: {celdaAtacada}</p>
+        )}
+      </div>
+
       {esJugador && barcosColocados && (
-      <button onClick={bloquearBarcos}>LISTO</button>
+        <button onClick={bloquearBarcos}>LISTO</button>
       )}
       <div className="contenedor-tablero">
-      <Numeros numeros={numeros} esJugador={esJugador} />
+        <Numeros numeros={numeros} esJugador={esJugador} />
         <div className="tablero-con-iconos">
           <FichasTablero
             fichas={[
@@ -233,7 +244,7 @@ const Tablero = ({esJugador, randomShips}) => {
                     ) ? 'celda-invalida' : ''}`}
                     onDragOver={(event) => handleDragOver(event, filaIndex, celdaIndex)}
                     onDrop={(event) => handleDrop(event, filaIndex, celdaIndex)}
-                    onClick={() => handleCellClick(filaIndex, celdaIndex)} 
+                    onClick={() => handleCellClick(filaIndex, celdaIndex)}
                   >
                     {celda && celda.icono && (
                       <img
