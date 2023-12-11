1e2b4b2c3979e32dc4cef1fa9c8a844f3a2d21e0
athanop <elnahue.-@live.com.ar>
2023-12-11 18:57:42 -0300

    ajustes y hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import PublicRoute from './PublicRoute';
 import MainScreen from "./components/pages/MainScreen";
 import GameScreen from "./components/pages/GameScreen";
@@ -7,12 +7,12 @@ import GameScreen from "./components/pages/GameScreen";
 const App = () => {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<PublicRoute > <MainScreen /> </PublicRoute>} />
         <Route path="/gamescreen" element={<PublicRoute > <GameScreen /> </PublicRoute>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
